e192cd53a5dbcd02ca222a047d06ecbd067fc8c0
andreafontaneto <andrea.fontaneto@gmail.com>
2022-01-25 15:23:18 +0100

    init project

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+
+<main class="container">
+  
+  <h1>HOME</h1>
+  
+</main>
+
+  
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+<header class="container py-3 mb-5">
+
+  <ul class="nav nav-pills">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="">HOME</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">TRIPS & TRAVEL</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="">CONTACTS</a>
+    </li>
+  </ul>
+
+</header>
